142cbbe5ea10425629e2608067ca8863fc5edc07
MGP-28 <joaosilvapereira96@hotmail.com>
2022-09-16 20:43:05 +0100

    Light/dark mode implemented

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import UserCard from "./components/UserCard.vue";
+import ColorModeToggle from "./components/ColorModeToggle.vue";
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <div class="dm-toggle-container-master">
+    <ColorModeToggle />
+  </div>
+  <div class="contentContainer">
+    <UserCard />
+  </div>
+</template>
+
+<style scoped>
+  .dm-toggle-container-master{
+    width: 100%;
+    height: max-content;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+  }
+</style>
---
+++ src/components/ColorModeToggle.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="dm-toggle-container" :class="(isDarkClass)">
+        <input 
+            type="checkbox" 
+            id="dm-toggle"
+            v-model="isDark"
+            >
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UserCard",
+        data(){
+            return{
+                isDark: true,
+            }
+        },
+        methods: {
+            toggleDark: function (){
+                document.documentElement.className = (document.documentElement.classList.contains('lightmode')) ? "" : "lightmode"
+            }
+        },
+        computed: {
+            isDarkClass(){
+                return this.isDark ? "toggled" : null
+            }
+        },
+        beforeCreate(){},
+        created(){},
+        beforeMount(){},
+        mounted(){},
+        beforeUpdate(){
+            this.toggleDark()
+        },
+        updated(){},
+        beforeUnmount(){},
+        unmounted(){}
+}
+</script>
+
+<style scoped>
+    .dm-toggle-container{
+        --height: 25px;
+        height: var(--height);
+        width: 45px;
+        padding: 0;
+        margin: 0;
+        background-color: var(--backgroundcolor);
+        border: var(--textcolor) solid 0.5px;
+        border-radius: var(--height);
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+    input{
+        appearance: none;
+        margin: 0 3px;
+        padding: 0;
+        cursor: pointer;
+        height: 18px;
+        width: 18px;
+        border-radius: 50%;
+        background-color: var(--titlecolor);
+    }
+    .dm-toggle-container.toggled{
+        justify-content: flex-end;
+    }
+</style>
